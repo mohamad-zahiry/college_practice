8abba3762616284d3a657638dfc86a1ec98ae9db
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-05-16 23:19:23 +0430

    Creating ArticleAdmin
    Adding prepopulated_fields

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(models.Article, ArticleAdmin)
