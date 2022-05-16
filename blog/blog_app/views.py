54e4c46666b09575a02bd95c38ebe8aadc7f969d
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-05-16 23:59:36 +0430

    Creating article home-page view

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from blog_app.models import Article
 
-# Create your views here.
+
+def all_articles(request):
+    articles = Article.objects.all()
+    return render(request, "articles/index.html", {"articles": articles})
