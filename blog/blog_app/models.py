9ec3f0c7442916f0512ee749026aacd30c26420a
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-05-16 23:16:45 +0430

    Creating Article model

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    slug = models.SlugField(max_length=200, unique=True, primary_key=True)
+    content = models.TextField()
+    publish_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
